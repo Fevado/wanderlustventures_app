74f17257e36cd55e01d465209afc933adfeed0c9
favoured mwange <mwangefavoured@gmail.com>
2026-02-02 15:04:05 +0300

    Updates logic of logIn & singUp

--- lib/screens/log_in.dart
+++ lib/screens/log_in.dart
@@ -4,7 +4,7 @@ import 'package:wanderlustventures_app/screens/home.dart';
 
 class LogIn extends StatefulWidget {
   const LogIn({super.key});
-  
+
   @override
   State<LogIn> createState() => _LogInState();
 }
@@ -17,134 +17,192 @@ class LogInForm extends StatefulWidget {
 }
 
 class _LogInFormState extends State<LogInForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _keepLoggedIn = false;
+
+  // dispose off controlers when widget is removed
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  // enable to the values when needed
+  void _submitForm() {
+    final String email = _emailController.text;
+    final String password = _passwordController.text;
+
+    // Added validation logic
+    if (email.isNotEmpty && password.isNotEmpty) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Text(
-              'Log In',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-        SizedBox(height: 5),
-        Row(children: [Text('Log In to continue your adventure.')]),
-        SizedBox(height: 10),
-        Row(
-          children: [
-            Text(
-              'Email',
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-            ),
-          ],
-        ),
-        SizedBox(height: 10),
-        TextField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            hintText: 'Enter Email',
-            hintStyle: TextStyle(fontSize: 15),
-            filled: true,
-            fillColor: const Color(0xFFC5E9FC),
-            contentPadding: EdgeInsets.only(left: 12),
-            prefixIcon: Icon(Icons.email_rounded),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(22)),
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'Log In',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ],
           ),
-        ),
-        SizedBox(height: 14),
-        Row(
-          children: [
-            Text(
-              'Password',
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-            ),
-          ],
-        ),
-        SizedBox(height: 10),
-        TextField(
-          keyboardType: TextInputType.visiblePassword,
-          decoration: InputDecoration(
-            hintText: 'Enter password',
-            hintStyle: TextStyle(fontSize: 15),
-            filled: true,
-            fillColor: const Color(0xFFC5E9FC),
-            contentPadding: EdgeInsets.only(left: 12),
-            prefixIcon: Icon(Icons.lock),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(22)),
+          SizedBox(height: 5),
+          Row(children: [Text('Log In to continue your adventure.')]),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Text(
+                'Email',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+              ),
+            ],
           ),
-        ),
-        SizedBox(height: 10),
-        Row(
-          children: [
-            Checkbox(
-              value: true,
-              activeColor: const Color(0xFF0498E5),
-              onChanged: (_) {},
-            ),
-            Text('Keep me logged in', textAlign: TextAlign.left),
-            // SizedBox(width: 68),
-            Spacer(),
-            Text(
-              'Forgot password?',
-              textAlign: TextAlign.right,
-              style: TextStyle(color: const Color(0xFF0498E5)),
-            ),
-          ],
-        ),
-
-        SizedBox(height: 14),
-        SizedBox(
-          width: double.infinity,
-          height: 46,
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Home()),
-              );
+          SizedBox(height: 10),
+          TextFormField(
+            controller: _emailController,
+            validator: (value) {
+              if (value == null || !value.contains('@')) {
+                return 'Enter a valid email address';
+              }
+              return null;
             },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFF0498E5),
-            ),
-            child: Text(
-              'Log In',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w600,
+            decoration: InputDecoration(
+              hintText: 'Enter Email',
+              hintStyle: TextStyle(fontSize: 15),
+              filled: true,
+              fillColor: const Color(0xFFC5E9FC),
+              contentPadding: EdgeInsets.only(left: 12),
+              prefixIcon: Icon(Icons.email_rounded),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(22),
               ),
             ),
           ),
-        ),
-
-        SizedBox(height: 20),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account yet?"),
-                SizedBox(width: 5),
-                // making the sign up navigable
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUp()),
-                    );
-                  },
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(color: const Color(0xFF0498E5)),
-                  ),
-                ),
-              ],
+          SizedBox(height: 14),
+          Row(
+            children: [
+              Text(
+                'Password',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            keyboardType: TextInputType.visiblePassword,
+            controller: _passwordController,
+            obscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Password is required';
+              }
+              if (value.length < 8) {
+                return 'Password must be 8 characters long';
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter password',
+              hintStyle: TextStyle(fontSize: 15),
+              filled: true,
+              fillColor: const Color(0xFFC5E9FC),
+              contentPadding: EdgeInsets.only(left: 12),
+              prefixIcon: Icon(Icons.lock),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(22),
+              ),
             ),
-          ],
-        ),
-      ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Checkbox(
+                value: _keepLoggedIn,
+                activeColor: const Color(0xFF0498E5),
+                onChanged: (bool? newValue) {
+                  setState(() {
+                    _keepLoggedIn = newValue ?? false;
+                  });
+                },
+              ),
+              Text('Keep me logged in', textAlign: TextAlign.left),
+              // SizedBox(width: 68),
+              Spacer(),
+              Text(
+                'Forgot password?',
+                textAlign: TextAlign.right,
+                style: TextStyle(color: const Color(0xFF0498E5)),
+              ),
+            ],
+          ),
+
+          SizedBox(height: 14),
+          SizedBox(
+            width: double.infinity,
+            height: 46,
+            child: ElevatedButton(
+              // onPressed: _submitForm,
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _submitForm();
+                }
+              },
+              // onPressed: () {
+              //   Navigator.push(
+              //     context,
+              //     MaterialPageRoute(builder: (context) => Home()),
+              //   );
+              // },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF0498E5),
+              ),
+              child: Text(
+                'Log In',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account yet?"),
+                  SizedBox(width: 5),
+                  // making the sign up navigable
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUp()),
+                      );
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(color: const Color(0xFF0498E5)),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -3,7 +3,7 @@ import 'package:wanderlustventures_app/screens/log_in.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
-  
+
   @override
   State<SignUp> createState() => _SignUpState();
 }
@@ -16,160 +16,232 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _passwordChars = false;
+
+  // disposing off controllers when widget is removed
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  // enabling access to the values when needed
+  void _submitForm() {
+    final String name = _nameController.text;
+    final String email = _emailController.text;
+    final String password = _passwordController.text;
+
+    // adding a validator
+    if (name.isNotEmpty && email.isNotEmpty && password.isNotEmpty) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => LogIn()));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordController.addListener(_updateCheckbox);
+  }
+
+  void _updateCheckbox() {
+    setState(() {
+      _passwordChars = _passwordController.text.length >= 8;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Text(
-              'Create an account',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-
-        SizedBox(height: 5),
-
-        Row(children: [Text('Set up your profile and travel smart.')]),
-
-        SizedBox(height: 10),
-
-        Row(
-          children: [
-            Text(
-              'Name',
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-            ),
-          ],
-        ),
-        SizedBox(height: 10),
-        TextField(
-          keyboardType: TextInputType.name,
-          decoration: InputDecoration(
-            hintText: 'Enter your name',
-            hintStyle: TextStyle(fontSize: 15),
-            filled: true,
-            fillColor: const Color(0xFFC5E9FC),
-            prefixIcon: Icon(Icons.person),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(22)),
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'Create an account',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ],
           ),
-        ),
 
-        SizedBox(height: 10),
+          SizedBox(height: 5),
 
-        Row(
-          children: [
-            Text(
-              'Email',
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-            ),
-          ],
-        ),
-        SizedBox(height: 10),
-        TextField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            hintText: 'Enter your Email',
-            hintStyle: TextStyle(fontSize: 15),
-            filled: true,
-            fillColor: const Color(0xFFC5E9FC),
-            prefixIcon: Icon(Icons.email_rounded),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(22)),
+          Row(children: [Text('Set up your profile and travel smart.')]),
+
+          SizedBox(height: 10),
+
+          Row(
+            children: [
+              Text(
+                'Name',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+              ),
+            ],
           ),
-        ),
-
-        SizedBox(height: 10),
-
-        Row(
-          children: [
-            Text(
-              'Password',
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
-            ),
-          ],
-        ),
-        SizedBox(height: 10),
-        TextField(
-          keyboardType: TextInputType.visiblePassword,
-          decoration: InputDecoration(
-            hintText: 'Enter Password',
-            hintStyle: TextStyle(fontSize: 15),
-            filled: true,
-            fillColor: const Color(0xFFC5E9FC),
-            prefixIcon: Icon(Icons.lock),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(22)),
-          ),
-        ),
-
-        SizedBox(height: 10),
-
-        Row(
-          children: [
-            Checkbox(
-              value: true,
-              activeColor: const Color(0xFF0498E5),
-              onChanged: (_) {},
-            ),
-            Text(
-              'Password must be 8 characters',
-              style: TextStyle(fontSize: 15),
-            ),
-          ],
-        ),
-
-        SizedBox(height: 10),
-
-        SizedBox(
-          width: double.infinity,
-          height: 46,
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LogIn()),
-              );
+          SizedBox(height: 10),
+          TextFormField(
+            controller: _nameController,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Enter name to proceed';
+              }
+              return null;
             },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFF0498E5),
-            ),
-            child: Text(
-              'Sign Up',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w600,
+            decoration: InputDecoration(
+              hintText: 'Enter your name',
+              hintStyle: TextStyle(fontSize: 15),
+              filled: true,
+              fillColor: const Color(0xFFC5E9FC),
+              prefixIcon: Icon(Icons.person),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(22),
               ),
             ),
           ),
-        ),
 
-        SizedBox(height: 20),
+          SizedBox(height: 10),
 
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Already have an account?'),
-                SizedBox(width: 5),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pop(
-                      context,
-                      MaterialPageRoute(builder: (context) => LogIn()),
-                    );
-                  },
-                  child: Text(
-                    'Log In',
-                    style: TextStyle(color: const Color(0xFF0498E5)),
-                  ),
-                ),
-              ],
+          Row(
+            children: [
+              Text(
+                'Email',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            controller: _emailController,
+            validator: (value) {
+              if (value == null || !value.contains('@')) {
+                return 'Please enter valid email address';
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter your Email',
+              hintStyle: TextStyle(fontSize: 15),
+              filled: true,
+              fillColor: const Color(0xFFC5E9FC),
+              prefixIcon: Icon(Icons.email_rounded),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(22),
+              ),
             ),
-          ],
-        ),
-      ],
+          ),
+
+          SizedBox(height: 10),
+
+          Row(
+            children: [
+              Text(
+                'Password',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          TextFormField(
+            controller: _passwordController,
+            obscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Password is required';
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter Password',
+              hintStyle: TextStyle(fontSize: 15),
+              filled: true,
+              fillColor: const Color(0xFFC5E9FC),
+              prefixIcon: Icon(Icons.lock),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(22),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 10),
+
+          Row(
+            children: [
+              Checkbox(
+                value: _passwordChars,
+                activeColor: const Color(0xFF0498E5),
+                onChanged: null,
+              ),
+              Text(
+                'Password must be 8 characters',
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
+
+          SizedBox(height: 10),
+
+          SizedBox(
+            width: double.infinity,
+            height: 46,
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _submitForm();
+                }
+              },
+              // onPressed: () {
+              //   Navigator.push(
+              //     context,
+              //     MaterialPageRoute(builder: (context) => LogIn()),
+              //   );
+              // },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF0498E5),
+              ),
+              child: Text(
+                'Sign Up',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Already have an account?'),
+                  SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(
+                        context,
+                        MaterialPageRoute(builder: (context) => LogIn()),
+                      );
+                    },
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(color: const Color(0xFF0498E5)),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
